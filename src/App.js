8a2ac783a76b80d37f05eb2ab3179ecee1877e38
Patrick Lee <cluster000@gmail.com>
2019-11-27 23:21:14 +0800

    Added project structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return <div className='App'>Bootcamp</div>;
-}
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Bootcamp</h1>
+    </div>
+  );
+};
 
 export default App;
