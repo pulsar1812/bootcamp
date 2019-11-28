258b725efca3102e4ec4c999e0e4c127c1b22e94
Patrick Lee <cluster000@gmail.com>
2019-11-28 23:24:44 +0800

    Added auth components, actions, and reducers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Routes from './components/routing/Routes';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
+import { loadUser } from './redux/actions/authActions';
+import store from './redux/store';
+import setAuthToken from './utils/setAuthToken';
 import './App.css';
 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
-    <div className='App'>
-      <h1>Bootcamp</h1>
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Landing} />
+          <Route component={Routes} />
+        </Switch>
+      </Fragment>
+    </Router>
   );
 };
 
